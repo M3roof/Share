4469bd4cf00fcde0bacc5e631118326b01fdb573
M3roof <43769947+M3roof@users.noreply.github.com>
2019-02-03 23:31:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,24 +12,7 @@ client.on('ready', () => {
 
 client.on('ready', async() => {
 var server = "516364082127831040"; // ايدي السررفر
-var channel = "524038276999413775";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
-    },305);
-})
-
-client.on('ready', async() => {
-var server = "516364082127831040"; // ايدي السررفر
-var channel = "524038276999413775";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
-    },305);
-})
-
- 
-client.on('ready', async() => {
-var server = "516364082127831040"; // ايدي السررفر
-var channel = "524038276999413775";//ايدي الروم
+var channel = "541717117926506517";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
@@ -39,57 +22,21 @@ var channel = "524038276999413775";//ايدي الروم
 
 client.on('ready', async() => {
 var server = "516364082127831040"; // ايدي السررفر
-var channel = "524038276999413775";//ايدي الروم
+var channel = "541717128131379204";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
 })
-
-
-
-
-
-
-
-
 
 
 client.on('ready', async() => {
 var server = "516364082127831040"; // ايدي السررفر
-var channel = "524038276999413775";//ايدي الروم
+var channel = "541717138142920745";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
 })
 
-
-
-
-
-
-
-
-client.on('ready', async() => {
-var server = "516364082127831040"; // ايدي السررفر
-var channel = "524038276999413775";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
-    },305);
-})
-
-
-
-
-
-
-
-client.on('ready', async() => {
-var server = "516364082127831040"; // ايدي السررفر
-var channel = "524038276999413775";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
-    },305);
-})
 
 
     client.on('message', message => {
